10f19d16258c5ff350e76ff559499b033754271d
basuabhirup <basu.abhirup27@gmail.com>
2025-02-01 23:38:37 +0530

    Fix "book-trial" button redirect

--- components/book-trial-section.tsx
+++ components/book-trial-section.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export default function BookTrialSection({ subject, course }: IProps) {
   return (
-    <section className="py-16 px-4 md:py-24 md:px-8 bg-usq-faded-peach">
+    <section className="py-32 px-4 md:px-8 bg-usq-faded-peach">
       <div className="container mx-auto">
         <div className="text-center mb-8">
           <h2 className="text-4xl md:text-5xl font-bold text-center mb-8 font-nunito text-usq-red">
